fee7948b67cba000c34d10102142bd0a190275e8
Musk <yuiop1029@naver.com>
2023-04-23 12:28:17 +0900

    Initial Commit

---
+++ SwiftCoin/SwiftCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinApp.swift
+//  SwiftCoin
+//
+//  Created by 유인호 on 2023/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
